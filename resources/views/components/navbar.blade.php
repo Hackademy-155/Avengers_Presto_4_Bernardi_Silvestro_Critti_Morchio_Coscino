16bd2c4fe6bcc390150f5c4078716d3e0b972018
Ylenia Bernardi <yleniabernardi1@gmail.com>
2024-12-05 14:50:57 +0100

    Navbar finita

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,43 @@
+<div class="fixed-top">
+    <div class="container-fluid navbar-container px-0">
+        <a class="navbar-brand" href="index.html">
+            <div class="title">
+                <h2>Presto.it</h2>
+            </div>
+        </a>
+        <nav class="navbar navbar-expand-lg navbar-freya navbar-light g-4 mx-auto">
+            <button class="navbar-toggler p-0 border-0" type="button" data-bs-toggle="collapse" data-bs-target="#primaryNavbarCollapse" aria-controls="primaryNavbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="hamburger hamburger--emphatic">
+                    <span class="hamburger-box">
+                        <span class="hamburger-inner"></span>
+                    </span>
+                </span>
+            </button>
+            <div class="collapse navbar-collapse" id="primaryNavbarCollapse">
+                <ul class="navbar-nav py-3 py-lg-0 mt-1 mb-2 my-lg-0 ms-lg-n1">
+                    <li class="nav-item ms-4"><a class="nav-link" href="#">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">About</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Products</a></li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle dropdown-indicator" href="JavaScript:void(0)" role="button" data-bs-toggle="dropdown" aria-expanded="false">Contact</a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="contact/contact1.html">Contact 1</a></li>
+                            <li><a class="dropdown-item" href="contact/contact2.html">Contact 2</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item"><a class="nav-link" href="#">Inserisci un annuncio</a></li>
+                </ul>
+                <ul class="navbar-nav ms-lg-auto flex-row justify-content-center py-3 py-lg-0 me-n2 mx-4">
+                    <li><a class="nav-link px-2 fs-5" href="#"><i class="bi bi-facebook"></i></a></li>
+                    <li><a class="nav-link px-2 fs-5" href="#"><i class="bi bi-twitter"></i></a></li>
+                    <li><a class="nav-link px-2 fs-5" href="#"><i class="bi bi-instagram"></i></a></li>
+                </ul>
+            </div>
+        </nav>
+        <div class="auth-buttons d-flex align-items-center justify-content-end">
+            <a href="#" class="btn-auth btn-login">Login</a>
+            <span class="divider"></span>
+            <a href="#" class="btn-auth btn-signup">Sign Up</a>
+        </div>
+    </div>
+</div>
